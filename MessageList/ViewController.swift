b1877644a0ee553f016f0cc7645864b49e8d9749
Tsuf <tifatsubasa@gmail.com>
2019-11-08 18:45:44 +0800

    Initial Commit

---
+++ MessageList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MessageList
+//
+//  Created by tsuf on 2019/11/8.
+//  Copyright © 2019 upmer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
